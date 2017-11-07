83a03ae93399383400f1da70c7497644f69626fd
pwolfe8 <pwolfe854@gmail.com>
2017-11-06 22:20:54 -0500

    added the shell for our high level python checker

---
+++ Python_high_level_testing/check_if_pattern_matches.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@date: 
+
+@author: 
+"""
+
+def check_if_pattern_matches(fixed_length_arr, len_used, stored_patterns):
+    # normalization
+    
+    # boundary calcs
+    
+    # check with boundaries and replace vals with pattern num
+    
+    # compare to our stored 4 patterns
+    
+    # output if each of 4 patterns matches or not
