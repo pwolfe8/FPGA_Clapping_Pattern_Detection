7c740f90fd4801f2953e2806ec99c9deaa98ae49
ZombieLinux <will@sutton-family.org>
2017-11-08 21:15:55 -0500

    Created sevenseg selector

--- VHDL/src/sevenseg.vhd
+++ VHDL/src/sevenseg.vhd
@@ -12,12 +12,13 @@ entity skeleton is
     );
     port (
         -- inputs --
+        patternIn :  in std_logic_vector ( 4 downto 0 );
         _name_		: in  _type_;
         -- outputs --
         --These are the 7seg display legs. 
-        seg : out std_logic_vector ( 6 downto 0 )
+        seg : out std_logic_vector ( 6 downto 0 );
        	--This is the selector for which of the 4 7seg displays get used. 
-        an : out std_logic_vector (4 downto 0 )
+        an : out std_logic_vector (4 downto 0 );
         _name_		: out _type_
     );
 end skeleton;
@@ -74,26 +75,30 @@ begin
 -- - = "0111111"
 -- _ = "1110111"
 
-
-      4'h0: ssOut = 7'b1000000;
-      4'h1: ssOut = 7'b1111001;
-      4'h2: ssOut = 7'b0100100;
-      4'h3: ssOut = 7'b0110000;
-      4'h4: ssOut = 7'b0011001;
-      4'h5: ssOut = 7'b0010010;
-      4'h6: ssOut = 7'b0000010;
-      4'h7: ssOut = 7'b1111000;
-      4'h8: ssOut = 7'b0000000;
-      4'h9: ssOut = 7'b0011000;
-      4'hA: ssOut = 7'b0001000;
-      4'hB: ssOut = 7'b0000011;
-      4'hC: ssOut = 7'b1000110;
-      4'hD: ssOut = 7'b0100001;
-      4'hE: ssOut = 7'b0000110;
-      4'hF: ssOut = 7'b0001110;
-      default: ssOut = 7'b1001001;
-
-
+	with patternIn select
+		seg <= "1000000" when "00000",
+			"0000110" when "00001",
+			"0100100" when "00010",
+			"0110000" when "00011",
+			"0011001" when "00100",
+			"0010010" when "00101",
+			"0000010" when "00110",
+			"1111000" when "00111",
+			"0000000" when "01000",
+			"0011000" when "01001",
+			"0001000" when "01010",
+			"0000011" when "01011",
+			"1000110" when "01100",
+			"0100001" when "01101",
+			"0000110" when "01110",
+			"0001110" when "01111",
+			"0100011" when "10000",
+			"0101011" when "10001",
+			"0001100" when "10010",
+			"1100011" when "10011",
+			"0111111" when "10100",
+			"1110111" when "10101",
+			"1111111" when others;
 
     -- component instantiations
     _name_ : entity work._entity_name_
