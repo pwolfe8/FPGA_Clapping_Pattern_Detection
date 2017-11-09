8fd6fad65ddabaf8627519b50d1968e9352f3048
ZombieLinux <will@sutton-family.org>
2017-11-08 22:19:08 -0500

    Initial completed sevenseg driver

--- VHDL/src/sevenseg.vhd
+++ VHDL/src/sevenseg.vhd
@@ -6,29 +6,35 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity skeleton is
+entity sevenseg is
     generic (
-        _name_			: _type_
+--        _name_			: _type_
     );
     port (
         -- inputs --
         patternIn :  in std_logic_vector ( 4 downto 0 );
-        _name_		: in  _type_;
+        clk : in std_logic; --Assuming 50Mhz clock for now. 
+        clapDetected : in std_logic;
+        clapFinished_buf: in std_logic
+        rst : in std_logic;
         -- outputs --
         --These are the 7seg display legs. 
         seg : out std_logic_vector ( 6 downto 0 );
        	--This is the selector for which of the 4 7seg displays get used. 
         an : out std_logic_vector (4 downto 0 );
-        _name_		: out _type_
     );
-end skeleton;
+end sevenseg;
 
-architecture skeleton_arch of skeleton is
+architecture sevenseg_arch of sevenseg is
     -- constant definitions
-    constant _name_ : _type_ := _value_;
+--    constant _name_ : _type_ := _value_;
     
     -- signal declarations
-    signal _name_ : _type_;
+--    signal _name_ : _type_;
+
+clockDiv : unsigned := 26;
+clapDetectedClk : std_logic;
+
 
 begin
     -- normal processes
@@ -38,6 +44,12 @@ begin
         elsif ( rising_edge(clk) ) then
             
         end if;
+    end process;
+
+    process ( clapDetectedClk, clapDetected ) begin
+    	if ( rising_edge(clk) and clapDetected = '1') then
+	    	seg <= "1110110" -- for 5 seconds
+    	end if;
     end process;
 
 -- Seven segment 
@@ -74,42 +86,46 @@ begin
 -- u = "1100011"
 -- - = "0111111"
 -- _ = "1110111"
-
-	with patternIn select
-		seg <= "1000000" when "00000",
-			"0000110" when "00001",
-			"0100100" when "00010",
-			"0110000" when "00011",
-			"0011001" when "00100",
-			"0010010" when "00101",
-			"0000010" when "00110",
-			"1111000" when "00111",
-			"0000000" when "01000",
-			"0011000" when "01001",
-			"0001000" when "01010",
-			"0000011" when "01011",
-			"1000110" when "01100",
-			"0100001" when "01101",
-			"0000110" when "01110",
-			"0001110" when "01111",
-			"0100011" when "10000",
-			"0101011" when "10001",
-			"0001100" when "10010",
-			"1100011" when "10011",
-			"0111111" when "10100",
-			"1110111" when "10101",
-			"1111111" when others;
+	process ( clapFinished_buf) begin
+		if (clapFinished_buf = '1') then
+			with patternIn select
+				seg <= "1000000" when "00000",
+					"0000110" when "00001",
+				"0100100" when "00010",
+				"0110000" when "00011",
+				"0011001" when "00100",
+				"0010010" when "00101",
+				"0000010" when "00110",
+				"1111000" when "00111",
+				"0000000" when "01000",
+				"0011000" when "01001",
+				"0001000" when "01010",
+				"0000011" when "01011",
+				"1000110" when "01100",
+				"0100001" when "01101",
+				"0000110" when "01110",
+				"0001110" when "01111",
+				"0100011" when "10000",
+				"0101011" when "10001",
+				"0001100" when "10010",
+				"1100011" when "10011",
+				"0111111" when "10100",
+				"1110111" when "10101",
+				"1111111" when others;
 
     -- component instantiations
-    _name_ : entity work._entity_name_
+    clockDiv : entity work.clockdiv
         generic map (
              -- map generics here
         )
         port map (
+        	clk,
+        	clapDetectedClk,
+        	clockDiv
             -- Paste entity's ports here. Connect signals in either form:
         --    "in1,in2,out1,out2"
         --    "portname1=>signal1, portname2=>signal2"
         );
-    
-    
-end skeleton_arch;
+
+   
+end sevenseg_arch;
