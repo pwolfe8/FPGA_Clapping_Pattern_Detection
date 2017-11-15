7df029d54f4d97e3a92319d491d68259b43d7a6f
ZombieLinux <will@sutton-family.org>
2017-11-15 17:44:15 -0500

    added inputs to divide by min

--- VHDL/src/div_by_min.vhd
+++ VHDL/src/div_by_min.vhd
@@ -9,10 +9,15 @@ use ieee.numeric_std.all;
 entity div_by_min is
     generic (
 --        _name_			: _type_
+	--Number of bits in the min value input
+	        R_int			: positive;
+	--Number of intervals
+		num_int 		: positive
+
     );
     port (
         -- inputs --
-
+	min_val : out std_logic_vector ( R_int downto 0)
         -- outputs --
     );
 end div_by_min;
