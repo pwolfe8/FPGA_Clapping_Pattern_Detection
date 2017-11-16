4f42c952f6d3e27b4b5aafc9064fed0089590c00
zombielinux <will@sutton-family.org>
2017-11-15 21:28:04 -0500

    figured out how to chop up the bank

--- VHDL/src/div_by_min.vhd
+++ VHDL/src/div_by_min.vhd
@@ -13,12 +13,13 @@ entity div_by_min is
 	        R_int			: positive;
 	--Number of intervals
 		num_int 		: positive
-
     );
     port (
         -- inputs --
+        bank    : in std_logic_vector   ( R_int*num_int-1 downto 0);
 	min_val : out std_logic_vector ( R_int downto 0)
         -- outputs --
+        bank_out    : in std_logic_vector   ( R_int*num_int-1 downto 0);
     );
 end div_by_min;
 
@@ -28,9 +29,15 @@ architecture div_by_min_arch of div_by_min is
     
     -- signal declarations
 --    signal _name_ : _type_;
-
+temp_numerator : std_logic_vector (R_int-1 downto 0);
 
 
 begin
-   
+	intervals: for i in 0 to num_int loop
+	--Get each chunk of the bank that we care about to do operations on. 
+		temp_numerator <= bank(R_int+R_int*i-1 downto R_int*i)
+			--min_val is even, therefore, we only have to bitshift by the largest power of 2, then by the rest?   
+
+
+
 end div_by_min_arch;
