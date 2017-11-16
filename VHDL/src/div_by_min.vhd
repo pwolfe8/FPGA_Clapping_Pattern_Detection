b6666fc0c2b64a1feee372eff96526753d97a6ab
zombielinux <will@sutton-family.org>
2017-11-15 22:12:43 -0500

    added division algorithm.... maybe, now with storing the useful output

--- VHDL/src/div_by_min.vhd
+++ VHDL/src/div_by_min.vhd
@@ -31,16 +31,17 @@ architecture div_by_min_arch of div_by_min is
 --    signal _name_ : _type_;
 numerator : unsigned (R_int-1 downto 0);
 divisor : unsigned (R_int downto 0);
-quotient : std_logic_vector (R_int+4 downto 0);
+quotient : std_logic_vector (R_int downto 0);
 
 begin
 	divisior <= unsigned(min_val);
 	intervals: for i in 0 to num_int loop
 	--Get each chunk of the bank that we care about to do operations on. 
 		numerator <= unsigned(resize(bank(R_int+R_int*i-1 downto R_int*i),R_int+1));
+		--Do the actual division below	
 		for j in 0 to R_int loop
 			quotient(R_int downto 1) := quotient(R_int-1 downto 0);
-			quotient(0) := numerator(a'length-1);	
+			quotient(0) := numerator(R_int);	
 			numerator(R_int downto 1) := numerator(R_int-1 downto 0);
 			quotient := quotient-divisor;
 			if(quotient(R_int-1) ='1') then
@@ -50,11 +51,8 @@ begin
 				numerator(0) :='1';	
 			end if;
 		end loop;
-		
-				
-
-unsigned(temp_numerator)/unsigned(min_val)
-
-
+		--Store the quotient result (no decimal precision WS 11-15-17) for output. 
+		bank_out(R_int+R_int*1-1 downto R_int*i) <= quotient;
+	
 
 end div_by_min_arch;
