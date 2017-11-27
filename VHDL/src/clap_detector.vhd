569ef136d511755e79e28b12707b588090a11dd8
Arceri, Matthew David II <Matthew.Arceri@gatech.edu>
2017-11-27 00:05:35 -0500

    Clap_detector compiles, type added for ADC res

--- VHDL/src/clap_detector.vhd
+++ VHDL/src/clap_detector.vhd
@@ -5,56 +5,50 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.TYPE_PACK.all;
 
 entity clap_detector is
     generic (
-        _name_			: _type_
+        R_int  : positive;
+        N_int  : positive;
+        N_patt : positive
     );
     port (
         -- inputs --
         clk, reset      : std_logic;
-        XADC		    : in  _type_; -- check on this from example XADC project...
+        XADC		    : in  unsigned(R_adc-1 downto 0); -- check on this from example XADC project...
         -- outputs --
         clap_detected	: out std_logic
     );
 end clap_detector;
 
-type signal_array is array (0 to 20) of 
-
-
-
 architecture clap_detector_arch of clap_detector is
     -- constant definitions
-    
+    type sig_array is array (0 to 20) of unsigned(R_adc-1 downto 0);
+
+    --signal signal_array is array (0 to 20) of unsigned(R_adc) <= ((others=> (others=>'0')));
     -- signal declarations
     
-
-process()
 begin
-    -- normal processes
-    
-    -- component instantiations
-    threshold <= 20000;
-    sum <= 0;
-    for i in 0 to 19 loop
-        sum <= signal_array(i) + sum;
-    end loop;
+    --Add entire array together
 
-    if sum > threshold then
-        clap_detected <= '1';
-    end if;
+    process(clk, reset)
+        variable sum : unsigned(R_adc+20 downto 0);
+        variable signal_array : sig_array  := ((others=> (others=>'0')));
+    begin
+        if(reset = '1') then
+            signal_array := ((others=> (others=>'0')));
+        elsif(rising_edge(clk)) then
+            signal_array := XADC & signal_array(0 to 19);
+            sum := (others => '0');
+            for n in signal_array'range loop
+                Sum := Sum + signal_array(n);                         
+            end loop;
 
-    for i in 20 to signal_array'LENGTH loop
-        sum <= sum + signal_array(i) - signal_array(i-20);
-        if sum > threshold then
-            clap_detected <= '1';
+            if sum > 20000 then
+                clap_detected <= '1';
+            end if;
         end if;
-    end loop;
-end process;
-
-
-
-
-    
-    
+    end process;
+  
 end clap_detector_arch;
